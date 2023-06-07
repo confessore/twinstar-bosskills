8076f76b5ead594e87331c294d728b9a87f44950
confessore <confessore@balasolu.com>
2023-06-07 12:32:19 -0400

    latest

--- components/latest_boss_kills_widget.tsx
+++ components/latest_boss_kills_widget.tsx
@@ -7,21 +7,25 @@ type Props = {
 
 export default function LatestBossKillsWidget(props: Props) {
   return (
-    <>
+    <div className="flex justify-center items-center">
       {props !== undefined && props.latest_boss_kills !== undefined && (
-        <>
+        <div className='flex flex-col m-1 p-1 w-64 justify-center items-center'>
           {props.latest_boss_kills.map((boss_kill, index) => {
-            return (
-              <div key={index} className="flex flex-wrap gap-3">
-                <p>{boss_kill.id?.toString()}</p>
-                <p>{boss_kill.realm?.toString()}</p>
-                <p>{boss_kill.guild?.toString()}</p>
-                <p>{boss_kill.time?.toString()}</p>
-              </div>
-            );
+            if (!boss_kill.hidden) {
+              return (
+                <div key={index} className='flex m-1 p-1'>
+                  <a href={`boss_kill/${boss_kill.id?.toString()}`} className='flex flex-col'>
+                    <p>{boss_kill.id?.toString()}</p>
+                    <p>{boss_kill.realm?.toString()}</p>
+                    <p>{boss_kill.guild?.toString()}</p>
+                    <p>{boss_kill.time?.toString()}</p>
+                  </a>
+                </div>
+              );
+            } else { return; }
           })}
-        </>
+        </div>
       )}
-    </>
+    </div>
   );
 }

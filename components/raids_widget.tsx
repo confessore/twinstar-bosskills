12373efb5860a0152e7ac76fd4faa9d63e9489d1
confessore <confessore@balasolu.com>
2023-06-25 22:20:00 -0400

    recent content first

--- components/raids_widget.tsx
+++ components/raids_widget.tsx
@@ -14,24 +14,6 @@ export default function RaidsWidget(props: Props) {
     <div className="m-0.5 flex w-96 max-w-full flex-col justify-center">
       <p className="m-0.5 text-xl font-bold">Raids</p>
       <div className="flex flex-col">
-        <p>Mogu&apos;shan Vaults</p>
-        {raids[0]?.map((value: number, index: number) => {
-          return <RaidCard key={index} value={value} mode={3} />;
-        })}
-        {raids[0]?.map((value: number, index: number) => {
-          return <RaidCard key={index} value={value} mode={4} />;
-        })}
-        {raids[0]?.map((value: number, index: number) => {
-          return <RaidCard key={index} value={value} mode={5} />;
-        })}
-        {raids[0]?.map((value: number, index: number) => {
-          return <RaidCard key={index} value={value} mode={6} />;
-        })}
-        {raids[0]?.map((value: number, index: number) => {
-          return <RaidCard key={index} value={value} mode={7} />;
-        })}
-      </div>
-      <div className="flex flex-col">
         <p>Heart of Fear</p>
         {raids[1]?.map((value: number, index: number) => {
           return <RaidCard key={index} value={value} mode={3} />;
@@ -46,6 +28,24 @@ export default function RaidsWidget(props: Props) {
           return <RaidCard key={index} value={value} mode={6} />;
         })}
         {raids[1]?.map((value: number, index: number) => {
+          return <RaidCard key={index} value={value} mode={7} />;
+        })}
+      </div>
+      <div className="flex flex-col">
+        <p>Mogu&apos;shan Vaults</p>
+        {raids[0]?.map((value: number, index: number) => {
+          return <RaidCard key={index} value={value} mode={3} />;
+        })}
+        {raids[0]?.map((value: number, index: number) => {
+          return <RaidCard key={index} value={value} mode={4} />;
+        })}
+        {raids[0]?.map((value: number, index: number) => {
+          return <RaidCard key={index} value={value} mode={5} />;
+        })}
+        {raids[0]?.map((value: number, index: number) => {
+          return <RaidCard key={index} value={value} mode={6} />;
+        })}
+        {raids[0]?.map((value: number, index: number) => {
           return <RaidCard key={index} value={value} mode={7} />;
         })}
       </div>

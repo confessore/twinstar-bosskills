49d6c3ac3df454628b2bcca07be4a38761270329
steven confessore <confessore@balasolu.com>
2023-07-02 10:48:53 -0400

    loot

--- pages/loot.tsx
+++ pages/loot.tsx
@@ -4,21 +4,24 @@ import { get_loot_count } from "@/utils/bosskills.db";
 import { Suspense } from "react";
 
 type Props = {
-  loot_count: number
+  item: any;
+  loot_count: number;
 };
 
 export const getServerSideProps = async (context: any) => {
+  let item;
   let loot_count;
   try {
+    item = await getItem(87209);
+    console.log(item);
     loot_count = await get_loot_count(87209);
-    console.log(loot_count)
   } catch {
     return {
       props: {},
     };
   }
   return {
-    props: { loot_count },
+    props: { item, loot_count },
   };
 };
 
@@ -27,9 +30,14 @@ export default function Loot(props: Props) {
     <Suspense fallback={Loading()}>
       {props !== undefined && props.loot_count !== undefined && (
         <div className="flex flex-wrap">
-          <div className="flex w-full flex-wrap justify-center">{props.loot_count} instances of 87209 exist</div>
+          <div className="flex w-full flex-wrap justify-center">{props.loot_count} instances of {props.item['itemSparse']['Name']} exist</div>
         </div>
       )}
     </Suspense>
   );
+}
+
+async function getItem(itemId: number) {
+  const res = await fetch(`https://twinstar-api.twinstar-wow.com/item/${itemId}`)
+  return res.json();
 }
